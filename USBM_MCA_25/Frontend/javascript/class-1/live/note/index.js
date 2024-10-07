a8b7c03f64872fff7805fda51fc6913c87f85276
Abhijitmalla <abhijitmalla06@gmail.com>
2024-10-07 12:00:14 +0530

    Add files via upload

---
+++ USBM_MCA_25/Frontend/javascript/class-1/live/note/index.js
@@ -0,0 +1,75 @@
+// console.log("hey i am abhi")
+// var age=25;
+// console.log(age);
+// const city="bbsr";
+// console.log(city);
+// let name="abhi";//declaration
+// console.log(name);
+// console.log(nam);
+// var nam;//declaration
+// nam="bhubaneswar";//initialisation
+// console.log(nam);//to print
+// let user=null;
+// console.log(typeof null);
+// let a=0;
+// console.log(typeof 0);
+// //undefined
+// let data;
+// console.log(typeof data);
+// console.log(data);
+// //number
+let num1=50;
+let decimal_no=1.0001;
+let pi=3.14;
+let ag=25;
+let roll_no=21;
+console.log(typeof num1);//number
+//addition
+let num2=457;
+let num3=37;
+let addition=num2+num3;
+console.log(addition);
+let num4=4;
+let num5=3;
+let num6=90;
+let num7=6;
+let substract=num4-num5;
+let mul=num5*num7;
+let div=num6/num5;
+console.log(substract);
+console.log(mul);
+console.log(div);
+//bigInt
+let num=6667777777777777777777777777777777777777777;
+console.log(typeof num);
+//symbol
+let sym1=Symbol("sym1");
+
+let sym2=Symbol("sym1");
+console.log(sym1==sym2);
+//nan
+let num9=0;
+let num19=0;
+console.log(num9/num19);//nan
+//int divided by string is also nan
+//number divide by zero give Infinity
+//boolean
+//it represents logical value true and false
+let ismarried=true;
+let issingle=false;
+console.log(ismarried);
+console.log(issingle);
+//String
+//collection of chars
+let nam='pramod';
+let cit="bbsr";
+console.log(nam,cit);
+
+let name1="abhijit";
+let age1=21;
+let rollno1=1;
+let college="usbm";
+console.log(`my name is ${name1} my age is ${age1} roll number is ${rollno1} college is ${college}`);
+
+
+
---
+++ USBM_MCA_25/Frontend/javascript/class-1/live/note/notes.md
@@ -0,0 +1,6 @@
+## how to push in git hub
+
+step-1 : 'git add.'
+undefined is a primitive data type used to denote a variable that has been declared but not assigned any value.
+number-it represents both int and float number in js.
+used for numeric calculation.
